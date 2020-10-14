d3384fe0e900e5040bde98d1efe34e370ccd05ab
Nikita Bakun <darty.vis@gmail.com>
2020-10-14 19:32:03 +0300

    paralax with bug

--- js/paralax.js
+++ js/paralax.js
@@ -1,5 +1,15 @@
-window.addEventListener('scroll',function (){
-    var value = window.scrollY;
-    document.getElementById("town").style.top = -value*0.5 +'px';
-})
+let ground = document.getElementById("ground");
+let car = document.getElementById("car");
+let water = document.getElementById("water");
+let bridge = document.getElementById("bridge");
+let town = document.getElementById("town");
 
+window.addEventListener('scroll', function (){
+    var value = window.scrollY;
+
+    town.style.top = value * 0.2 + 'px';
+    bridge.style.top = -value * 0.1 + 'px';
+    water.style.top = -value * 0.1 + 'px';
+    car.style.top = -value * 0.4 + 'px';
+    ground.style.top = -value * 0.4 + 'px'
+})
