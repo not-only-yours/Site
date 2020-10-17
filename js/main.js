619704130628c9a28bf60a80785ccd7acef5d9d7
Nikita Bakun <darty.vis@gmail.com>
2020-10-17 19:11:09 +0300

    fixed bugs text buttons configurator

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-menu.onclick = function myFunction() {
+$(window).on('beforeunload', function (){
+    $(window).scrollTop(0);
+})
+
+menu.onclick = function() {
     var x =document.getElementById('myTopnav');
     if(x.className ==="topnav"){
         x.className+=" responsive";
@@ -6,27 +10,69 @@ menu.onclick = function myFunction() {
         x.className = "topnav";
 }
 
-document.getElementById("rightFirstButton").onclick = function func(){
+
+
+document.getElementById("rightFirstButton").onclick = function(){
     document.getElementById("rightFirstButton").style.display = "none";
     document.getElementById("rightDownMenu").style.display = "flex";
 }
 
-document.getElementById("close").onclick = function fun(){
+document.getElementById("close").onclick = function(){
     document.getElementById("rightFirstButton").style.display = "block";
     document.getElementById("rightDownMenu").style.display = "none";
 }
 
-
-document.getElementById("firstConfBut").onclick = function fq(){
+function first(){
+    document.getElementById("conf").style.display = "none";
+    document.getElementById("displayInfoAboutCar").style.display = "flex";
     document.getElementById("confFirst").style.display = "flex";
     document.getElementById("confSecond").style.display = "none";
     document.getElementById("confThird").style.display = "none";
     document.getElementById("confFouth").style.display = "none";
 }
 
-document.getElementById("SecondConfBut").onclick = function fw(){
+ function second(){
+     document.getElementById("conf").style.display = "none";
+     document.getElementById("displayInfoAboutCar").style.display = "flex";
     document.getElementById("confFirst").style.display = "none";
     document.getElementById("confSecond").style.display = "flex";
     document.getElementById("confThird").style.display = "none";
     document.getElementById("confFouth").style.display = "none";
 }
+
+function third(){
+    document.getElementById("conf").style.display = "none";
+    document.getElementById("displayInfoAboutCar").style.display = "flex";
+    document.getElementById("confFirst").style.display = "none";
+    document.getElementById("confSecond").style.display = "none";
+    document.getElementById("confThird").style.display = "flex";
+    document.getElementById("confFouth").style.display = "none";
+}
+
+function fouth(){
+    document.getElementById("conf").style.display = "none";
+    document.getElementById("displayInfoAboutCar").style.display = "flex";
+    document.getElementById("confFirst").style.display = "none";
+    document.getElementById("confSecond").style.display = "none";
+    document.getElementById("confThird").style.display = "none";
+    document.getElementById("confFouth").style.display = "flex";
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function seeCar(){
+    document.getElementById("cars").scrollIntoView({ block:"center",behavior: 'wrapper' });
+}
+
+function seeConf(){
+    document.getElementById("displayInfoAboutCar").scrollIntoView({ block:"center",behavior: 'wrapper' });
+}
+
+window.addEventListener('scroll', function() {
+    var value = window.scrollY;
+    document.getElementById("topMenuOne").style.top = -value;
+
+});
